204be08245338696e40ccebd665ec5c955dd9116
androosk <atirpok3@gmail.com>
2021-04-16 14:20:54 -0600

    finished form validation added resume button

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/emailValidate';
 
 const ContactForm = () => {
-  const handleSubmit = {
-    // do submitty things in here
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const { name, email, message } = formState;
+
+  const handleChange = (e) => {
+    if(e.target.name === 'email'){
+      const isValid = validateEmail(e.target.value);
+      if(!isValid) {
+        setErrorMessage('Your email is invalid')
+      } else {
+        setErrorMessage('')
+      }
+    } else {
+      if(!e.target.value.length){
+        setErrorMessage(`${e.target.name} is required`)
+      } else {
+        setErrorMessage('')
+      }
+    }
+    if(!errorMessage){
+      setFormState({
+        ...formState,
+        [e.target.name]: e.target.value
+      })
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formState)
   }
   
   return (
@@ -11,18 +46,23 @@ const ContactForm = () => {
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name"></label>
-          <input type="text" name="name" placeholder="name" />
+          <input type="text" defaultValue={name} onBlur={handleChange} name="name" placeholder="name" />
         </div>
         <div>
           <label htmlFor="email"></label>
-          <input type="email" name="email" placeholder="email"/>
+          <input type="email" defaultValue={email} onBlur={handleChange} name="email" placeholder="email"/>
         </div>
         <div>
           <label htmlFor="message"></label>
-          <textarea name="message" rows="5" placeholder="message"/>
+          <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" placeholder="message"/>
         </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
         <button type="submit">send request</button>
-        <a className="resume-button" target="_blank" href="https://www.dropbox.com/scl/fi/xo3g9f47psav12fqmme70/Andrew-Tirpok-Resume-2021.docx?dl=0&rlkey=lm6mt82rp03olfum6egvtqy51">download resume</a>
+        <a className="resume-button" target="_blank" rel="noreferrer" href="https://www.dropbox.com/scl/fi/xo3g9f47psav12fqmme70/Andrew-Tirpok-Resume-2021.docx?dl=0&rlkey=lm6mt82rp03olfum6egvtqy51">download resume</a>
       </form>
     </section>
   )
