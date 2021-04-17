f87d3e838d728c3322654f34a3c5d363bcaa5831
androosk <atirpok3@gmail.com>
2021-04-17 12:32:40 -0600

    fixed a minor error with modal anchor tags

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Modal = ({ onClose, currentPhoto }) => {
-  const { name, description, github, deployment, index } = currentPhoto;
+  const { name, description, caption, github, deployment, index } = currentPhoto;
 
   return (
     <div className="modal-backdrop">
       <div className="modal-container">
         <h3 className="modal-title">{name}</h3>
-        <img src={require(`../../assets/projects/${index}.jpg`).default} alt="current photo" />
+        <img src={require(`../../assets/projects/${index}.jpg`).default} alt={caption} />
         <p>{description}</p>
         <a className="modal-button" target="_blank" href={github} rel="noreferrer">GitHub Repo</a>
         <a className="modal-button" target="_blank" href={deployment} rel="noreferrer">Deployed Application</a>
